86c092c09ffa30c58fda7fe4d627ab1d226e96d1
Anthony <huggins9000211@gmail.com>
2019-09-26 18:49:22 -0400

    test

--- opcodeFuncs2.c
+++ opcodeFuncs2.c
@@ -2,47 +2,47 @@
 
 void sub(stack_t **head, unsigned int counter)
 {
-    stack_t *temp;
-    int number = 0;
-    if ((*head)->next == NULL)
-    {
-        fprintf(stderr, "L%u: can't sub, stack too short", counter);
-        exit(EXIT_FAILURE);
-    }
-    number = number + (*head)->n;
-    temp = (*head)->next;
-    temp->n = temp->n - number;
-    (*head) = temp;
+	stack_t *temp;
+	int number = 0;
+	if ((*head)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't sub, stack too short", counter);
+		exit(EXIT_FAILURE);
+	}
+	number = number + (*head)->n;
+	temp = (*head)->next;
+	temp->n = temp->n - number;
+	(*head) = temp;
 }
 void mul(stack_t **head, unsigned int counter)
 {
-    stack_t *temp;
-    int number = 0;
-    if ((*head)->next == NULL)
-    {
-        fprintf(stderr, "L%u: can't mul, stack too short", counter);
-        exit(EXIT_FAILURE);
-    }
-    number = number + (*head)->n;
-    temp = (*head)->next;
-    temp->n = temp->n * number;
-    (*head) = temp;
+	stack_t *temp;
+	int number = 0;
+	if ((*head)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't mul, stack too short", counter);
+		exit(EXIT_FAILURE);
+	}
+	number = number + (*head)->n;
+	temp = (*head)->next;
+	temp->n = temp->n * number;
+	(*head) = temp;
 }
 void pchar(stack_t **head, unsigned int counter)
 {
-    int number = (*head)->n;
-    if ((*head) == NULL)
-    {
-        fprintf(stderr, "L%u: can't pchar, stack empty", counter);
-        exit(EXIT_FAILURE);
-    }
-    else if ((number < 0) || (number > 127))
-    {
-        fprintf(stderr, "L%u: can't pchar, value out of range", counter);
-        exit(EXIT_FAILURE);
-    }
-    
-    printf("%c\n", number);
+	int number = (*head)->n;
+	if ((*head) == NULL)
+	{
+		fprintf(stderr, "L%u: can't pchar, stack empty", counter);
+		exit(EXIT_FAILURE);
+	}
+	else if ((number < 0) || (number > 127))
+	{
+		fprintf(stderr, "L%u: can't pchar, value out of range", counter);
+		exit(EXIT_FAILURE);
+	}
+	
+	printf("%c\n", number);
 }
 void pstr(stack_t **head, unsigned int counter)
 {
@@ -54,18 +54,18 @@ void pstr(stack_t **head, unsigned int counter)
 	while ((temp != NULL) && (temp->n != 0) && ((temp->n > 0) && (temp->n < 127)))
 	{
 		int number = temp->n;
-        if (temp == NULL)
-        {
-            fprintf(stderr, "L%u: can't pchar, stack empty", counter);
-            exit(EXIT_FAILURE);
-        }
-        else if ((number < 0) || (number > 127))
-        {
-            fprintf(stderr, "L%u: can't pchar, value out of range", counter);
-            exit(EXIT_FAILURE);
-        }
-        printf("%c", number);
+		if (temp == NULL)
+		{
+			fprintf(stderr, "L%u: can't pchar, stack empty", counter);
+			exit(EXIT_FAILURE);
+		}
+		else if ((number < 0) || (number > 127))
+		{
+			fprintf(stderr, "L%u: can't pchar, value out of range", counter);
+			exit(EXIT_FAILURE);
+		}
+		printf("%c", number);
 		temp = temp->next;
 	}
-    printf("\n");
+	printf("\n");
 }
